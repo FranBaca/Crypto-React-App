f08bfa1c57c7931a246d83b2070334b78548e838
FranBaca <franbaca13@gmai.com>
2022-06-29 23:57:47 -0300

    Add project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import CryptoContext from './CryptoContext';
+import 'react-alice-carousel/lib/alice-carousel.css';
+ReactDOM.render(
+  <CryptoContext>
+    <App/>,
+  </CryptoContext>,
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
